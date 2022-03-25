dca6c6cb2f40a1a32b3bb283eab81e8c0c4e8e8c
rayray2002 <rayray2002.huang@gmail.com>
2022-03-26 02:54:42 +0800

    momentum

--- Midterm/code/test.py
+++ Midterm/code/test.py
@@ -1,15 +1,13 @@
 import numpy as np
-# from pprint import pprint
 
-# a = [-1, 6, -13, 20, -13, 6, -1]
+a = [-1, 6, -13, 20, -13, 6, -1]
 
-# A_tmp = np.zeros((16, 22))
+A_tmp = np.zeros((16, 22))
 
-# for i in range(16):
-#     A_tmp[i, i : i + 7] = a
+for i in range(16):
+    A_tmp[i, i : i + 7] = a
 
-# A = A_tmp[:, 3:-3]
-# pprint(A)
+A = A_tmp[:, 3:-3]
 
 b = np.zeros(22, dtype=np.float64)
 
@@ -28,9 +26,10 @@ with open("pattern1.dat") as f:
 
 x_gs = b / 20
 x_j = b / 20
+x_ans = b / 20
+lamb = 0.5
 
-for i in range(1000):
-    # x_new = np.zeros(22)
+for i in range(256):
     x_j[3:19] = (
         (
             b
@@ -39,7 +38,7 @@ for i in range(1000):
             + (np.roll(x_j, -3) + np.roll(x_j, 3))
         )
         / 20
-    )[3:19]
+    )[3:19] * lamb + x_j[3:19] * (1 - lamb)
 
     for j in range(3, 19):
         x_gs[j] = (
@@ -49,5 +48,15 @@ for i in range(1000):
             + (x_gs[j + 3] + x_gs[j - 3])
         ) / 20
 
-print(x_j)
-print(x_gs)
+for i in range(3000):
+    for j in range(3, 19):
+        x_ans[j] = (
+            b[j]
+            + 13 * (x_ans[j + 1] + x_ans[j - 1])
+            - 6 * (x_ans[j + 2] + x_ans[j - 2])
+            + (x_ans[j + 3] + x_ans[j - 3])
+        ) / 20
+
+print(np.square(A@x_ans[3:19]-b[3:19]).sum())
+print(np.square(A@x_gs[3:19]-b[3:19]).sum())
+print(np.square(A@x_j[3:19]-b[3:19]).sum())
