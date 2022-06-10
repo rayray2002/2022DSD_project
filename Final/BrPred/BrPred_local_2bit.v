a2e10a6e80aa40ec9ab0b31b096cc12325e68ee4
rayray2002 <rayray2002.huang@gmail.com>
2022-06-10 21:24:30 +0800

    branch prediction

--- Final/BrPred/BrPred_local_2bit.v
+++ Final/BrPred/BrPred_local_2bit.v
@@ -1,13 +1,13 @@
 module BrPred_local_2bit(
     input                  clk,
-    input                  rst,
+    input                  rst_n,
+    input branch,
 
     input                  BranchTaken_i,
+    input                  miss,
     input         [31:0]   WriteAddr_i,
-    input         [31:0]   WriteTarget_i,
 
     input         [31:0]   ReadAddr_i,
-    output        [31:0]   ReadTarget_o,
     output                 Hit_o
 );
 
@@ -19,8 +19,6 @@ localparam S_NEAR_NONTAKEN = 1;
 localparam S_NEAR_TAKEN    = 2;
 localparam S_TAKEN         = 3;
 
-reg [31:0] target      [0:NUM_ENTRY-1];
-reg [31:0] target_nxt  [0:NUM_ENTRY-1];
 
 reg [1:0]  predict     [0:NUM_ENTRY-1];
 reg [1:0]  predict_nxt [0:NUM_ENTRY-1];
@@ -33,61 +31,46 @@ integer i;
 
 //// Combinational ////
 
-assign ReadTarget_o = target[read_index];
 
-assign Hit_o =
-    predict[read_index] == S_TAKEN
-    || predict[read_index] == S_NEAR_TAKEN;
+assign Hit_o = branch &
+    (predict[read_index] == S_TAKEN
+    || predict[read_index] == S_NEAR_TAKEN);
 
 assign read_index = ReadAddr_i[NUM_INDEX_BIT+1:2];
 assign write_index = WriteAddr_i[NUM_INDEX_BIT+1:2];
 
 always @* begin
     for (i = 0; i < NUM_ENTRY; i = i + 1) begin
-        target_nxt[i] = target[i];
-    end
-
-    if (BranchTaken_i) begin
-        target_nxt[write_index] = { WriteTarget_i[31:2], 2'b00 };
-    end
-end
-
-always @* begin
-    for (i = 0; i < NUM_ENTRY; i = i + 1) begin
         predict_nxt[i] = predict[i];
     end
 
-    if (BranchTaken_i) begin
-        case (predict[write_index])
-            S_NONTAKEN: begin
-                predict_nxt[write_index] = S_NEAR_NONTAKEN;
-            end
-            S_NEAR_NONTAKEN: begin
-                predict_nxt[write_index] = S_NEAR_TAKEN;
-            end
-            S_NEAR_TAKEN: begin
-                predict_nxt[write_index] = S_TAKEN;
-            end
-            S_TAKEN: begin
-                predict_nxt[write_index] = S_TAKEN;
-            end
-        endcase
-    end
-    else begin
-        case (predict[write_index])
-            S_NONTAKEN: begin
-                predict_nxt[write_index] = S_NONTAKEN;
-            end
-            S_NEAR_NONTAKEN: begin
-                predict_nxt[write_index] = S_NONTAKEN;
-            end
-            S_NEAR_TAKEN: begin
-                predict_nxt[write_index] = S_NEAR_NONTAKEN;
-            end
-            S_TAKEN: begin
-                predict_nxt[write_index] = S_NEAR_TAKEN;
-            end
-        endcase
+    if (miss) begin
+        if (BranchTaken_i) begin
+            case (predict[write_index])
+                S_NONTAKEN, S_NEAR_NONTAKEN: begin
+                    predict_nxt[write_index] = S_NEAR_NONTAKEN;
+                end
+                S_NEAR_NONTAKEN: begin
+                    predict_nxt[write_index] = S_NEAR_TAKEN;
+                end
+                default: begin
+                    predict_nxt[write_index] = S_TAKEN;
+                end
+            endcase
+        end
+        else begin
+            case (predict[write_index])
+                default: begin
+                    predict_nxt[write_index] = S_NONTAKEN;
+                end
+                S_NEAR_TAKEN: begin
+                    predict_nxt[write_index] = S_NEAR_NONTAKEN;
+                end
+                S_TAKEN: begin
+                    predict_nxt[write_index] = S_NEAR_TAKEN;
+                end
+            endcase
+        end
     end
 end
 
@@ -95,13 +78,13 @@ end
 
 always @(posedge clk) begin
     for (i = 0; i < NUM_ENTRY; i = i + 1) begin
-        if (rst) begin
-            predict[i] <= 0;
-            target[i] <= 0;
+        if (~rst_n) begin
+            predict[i] <= S_NEAR_TAKEN;
         end
         else begin
-            predict[i] <= predict_nxt[i];
-            target[i] <= target_nxt[i];
+            if (miss) begin
+                predict[i] <= predict_nxt[i];
+            end
         end
     end
 end
---
+++ Final/BrPred/base_module/ALU.v
@@ -0,0 +1,58 @@
+// ALU ctrl
+`define ADD 4'b0010
+`define SUB 4'b0110
+`define AND 4'b0000
+`define OR  4'b0001
+`define XOR 4'b0011
+`define SLT 4'b1000
+`define SLL 4'b1001
+`define SRL 4'b1010
+`define SRA 4'b1011
+
+module ALU(
+    data1_i,
+    data2_i,
+    ALUCtrl_i,
+    data_o,
+    Zero_o
+);
+
+input signed [31:0] data1_i; 
+input signed [31:0] data2_i;
+input  [3:0]  ALUCtrl_i;
+output reg signed [31:0] data_o;
+output Zero_o;
+
+wire signed [31:0] Adder_o;
+wire [32:0] data1_extend;
+wire [32:0] data2_extend;
+wire overflow;
+wire neg;
+
+assign neg = ALUCtrl_i[3] || ALUCtrl_i[2];
+
+assign data1_extend = {data1_i[31], data1_i};
+assign data2_extend = {data2_i[31], data2_i} ^ {33{neg}};
+
+assign {overflow, Adder_o} = data1_extend + data2_extend + neg;
+
+assign Zero_o = (data_o == 0);
+
+always @(*) begin
+    case(ALUCtrl_i)
+        `ADD, `SUB: data_o = Adder_o;
+        `AND: data_o = data1_i & data2_i;
+        `OR: data_o = data1_i | data2_i;
+        `XOR: data_o = data1_i ^ data2_i;
+        `SLT: data_o = overflow;
+        // `SLT: data_o = data1_i < data2_i;
+    
+        `SLL: data_o = data1_i << data2_i[4:0];
+        `SRL: data_o = data1_i >> data2_i[4:0];
+        `SRA: data_o = data1_i >>> data2_i[4:0];
+
+        default: data_o = Adder_o;
+    endcase
+end
+
+endmodule
